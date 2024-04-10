3a9b96dc69298cf04f5f021200a3f1602f9fa3fd
Serhii Krasovskyi <chokorik@gmail.com>
2024-04-10 18:30:50 +0300

    HW_2

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,23 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum DaysWeeks {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function isWeekend(daysWeeks: DaysWeeks): boolean {
+  if (daysWeeks === DaysWeeks.Saturday || daysWeeks === DaysWeeks.Sunday) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+export {};
--- src/generic/7.ts
+++ src/generic/7.ts
@@ -4,16 +4,21 @@
 */
 
 export enum UserRole {
-  admin = 'admin',
-  editor = 'editor',
-  guest = 'guest',
+  admin = "admin",
+  editor = "editor",
+  guest = "guest",
 }
 
 // Замініть наступний код на версію за допомогою Record
-const RoleDescription = {
-  admin: 'Admin User',
-  editor: 'Editor User',
-  guest: 'Guest User',
+
+type Role = "admin" | "editor" | "guest";
+
+type RoleValue = Record<Role, string>;
+
+const RoleDescription: RoleValue = {
+  admin: "Admin User",
+  editor: "Editor User",
+  guest: "Guest User",
 };
 
 export {};
